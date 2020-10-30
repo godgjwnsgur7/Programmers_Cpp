d4e32470be6d7afe4bc95bb6bed49bae5953a219
허준혁 <a33879702@gmail.com>
2020-10-30 12:11:34 +0900

    commit

--- Programmers Lv1/Mock_Test.cpp
+++ Programmers Lv1/Mock_Test.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,39 +19,35 @@ using namespace std;
 vector<int> solution(vector<int> answers)
 {
     vector<int> answer;
-    int one[] = {1, 2, 3, 4, 5};
-    int two[] = {2, 1, 2, 3, 2, 4, 2, 5};
-    int three[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    int score[3] = {0};
+    vector<int> one = {1, 2, 3, 4, 5};
+    vector<int> two = {2, 1, 2, 3, 2, 4, 2, 5};
+    vector<int> three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    vector<int> score = {0, 0, 0};
 
     int size = answers.size();
     for (int i = 0; i < size; i++)
     {
-        if (answers[i] == one[i % 5])
+        if (answers[i] == one[i % one.size()])
         {
             score[0]++;
         }
 
-        if (answers[i] == two[i % 8])
+        if (answers[i] == two[i % two.size()])
         {
             score[1]++;
         }
 
-        if (answers[i] == three[i % 10])
+        if (answers[i] == three[i % three.size()])
         {
             score[2]++;
         }
     }
 
-    int max = score[0];
-    if (max <= score[1])
-        max = score[1];
-    if (max <= score[2])
-        max = score[2];
+    int maxnum = *max_element(score.begin(), score.end());
 
     for (int i = 0; i < 3; i++)
     {
-        if (max == score[i])
+        if (maxnum == score[i])
             answer.push_back(i + 1);
     }
 
